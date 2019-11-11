77b7c0d0302fa943fb86ac56a843ed2f3ad2f9b7
Student <student@donation.local>
2019-11-11 12:58:45 -0600

    made the models for the application via the README

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.TextField()
+    rating = models.IntegerField()
+    genre = models.TextField()
+    runtime = models.CharField()
+
+class Showing(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    showtime = models.TimeField()
+
+class Ticket(models.Model):
+    name = models.TextField()
+    purchased_at = models.DateTimeField()
+    showing = models.ForeignKey(Showing, on_delete=models.PROTECT)
